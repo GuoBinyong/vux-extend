5012d59e351ccdbeab29aaf7b5dc9832d86d7cb9
郭斌勇 <guobinyong@qq.com>
2020-05-19 15:45:04 +0800

    完成配置和开发

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "globalThis":"readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // 允许 debugger 运行在 开发 环境中
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 关闭 var 关键字的提示
+        "no-var":'off',
+        // 警告 case 穿透
+        "no-fallthrough":'warn'
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "xdialog-fix";
+export * from "custom-viewbox";
+export {default as customViewBox} from "custom-viewbox";
+export * from "viewbox-data-handler";
+export * from "xheader-extend";
